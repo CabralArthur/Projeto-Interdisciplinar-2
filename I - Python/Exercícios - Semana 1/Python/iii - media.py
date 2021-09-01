b498a84165bdf16155befd02c5431bcda44ab1b9
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-09-01 14:24:07 -0300

    📚 - Média

---
+++ I - Python/Exercícios - Semana 1/Python/iii - media.py
@@ -0,0 +1,12 @@
+""" 
+Calcular a média aritmética de três notas 
+fornecidas pelo usuário.
+"""
+
+nota1 = float(input("Insira a primeira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+nota3 = float(input("Insira a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print("A média entre as notas é:", media)
