8dc7593acec01a25a393e33ecb079d49f5a20d5f
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-09-01 14:23:19 -0300

    ✖️ - Multiplicação

---
+++ I - Python/Exercícios - Semana 1/Python/ii - mult.py
@@ -0,0 +1,11 @@
+"""
+Exibir a multiplicação de dois números 
+reais informados pelo usuário.
+"""
+
+num1 = float(input('Insira aqui um número: '))
+num2 = float(input('Insira aqui outro número: '))
+
+mult = num1 * num2
+
+print('A multiplicação desses números é', mult)
