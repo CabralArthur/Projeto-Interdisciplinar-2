dbb85118513101776eac0c2705d777d83a620726
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-09-01 14:27:04 -0300

    💸 - Salário

---
+++ I - Python/Exercícios - Semana 1/Python/v - salario.py
@@ -0,0 +1,11 @@
+""" 
+Faça um programa para ler o salário de um 
+funcionário e aumentá-lo em 20%. Imprima 
+seu salário final.
+"""
+
+salario = float(input('Isira aqui o salário do funcionário: '))
+
+salario += salario * 0.2
+
+print("O salário novo desse funcionário é R$", salario)
