1b421d76c48e0a56f78c89f99187c4105c63ae05
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2021-10-12 11:09:56 -0300

    Updating Repo

--- I - Python/Exercícios - Semana 2/Python/i - par_ou_impar.py
+++
@@ -1,6 +0,0 @@
-num = int(input('Insira aqui o número: '))
-
-if num % 2 == 0:
-    print('Esse número é par')
-else:
-    print('Esse número é impar')
--- I - Python/Exercícios - Semana 2/Python/ii - temperatura.py
+++
@@ -1,8 +0,0 @@
-temp = float(input('Insira aqui a temperatura: '))
-
-if temp >= 39:
-    print('Febre Alta')
-elif temp >= 37 and temp > 39:
-    print('Febril')
-else:
-    print('Sem febre')
